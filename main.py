04dc3bdacf53f77f365889b18e66f171728909a9
Vincent <vincenttse0531@gmail.com>
2025-07-11 00:34:16 +0100

    changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 from dotenv import load_dotenv
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from telegram.ext import PicklePersistence
 from commands.game_admin import new_game, start_game, kick
 from commands.pre_game_actions import join, leave
 from commands.language import language, set_language_callback
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 def main():
+    load_dotenv()
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
     os.makedirs("data", exist_ok=True)
     persistence = PicklePersistence(filepath="data/bot_data.pkl")
     app = ApplicationBuilder().token(BOT_TOKEN).persistence(persistence).build()
@@ -21,9 +22,9 @@ def main():
     app.add_handler(CommandHandler("kick", kick))
     app.add_handler(CommandHandler("language", language))
     app.add_handler(CallbackQueryHandler(set_language_callback, pattern="^lang_"))
-
+    
     print("Bot is running...")
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
